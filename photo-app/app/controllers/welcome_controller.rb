9a20e531d98fb694594acb2dce759939ce0d5d07
zakevicius <m.zakevicius@gmail.com>
2021-01-01 20:22:34 +0200

    Email confirmation added

--- photo-app/app/controllers/welcome_controller.rb
+++ photo-app/app/controllers/welcome_controller.rb
@@ -2,5 +2,6 @@ class WelcomeController < ApplicationController
   skip_before_action :authenticate_user!, only: ['index']
   
   def index
+
   end
 end
--- photo-app/config/environments/development.rb
+++ photo-app/config/environments/development.rb
@@ -30,6 +30,9 @@ Rails.application.configure do
     config.cache_store = :null_store
   end
 
+  config.action_mailer.delivery_method = :test
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000, protocol: 'http' }
+
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
 
