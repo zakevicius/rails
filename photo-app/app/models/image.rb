541b554efd51ddacc21d2fe86739158c934bc901
zakevicius <m.zakevicius@gmail.com>
2021-01-02 19:51:24 +0200

    Final configs for production

--- photo-app/app/models/image.rb
+++ photo-app/app/models/image.rb
@@ -1,4 +1,13 @@
 class Image < ApplicationRecord
   belongs_to :user
   mount_uploader :picture, PictureUploader
+  validate :picture_size
+
+  private
+
+  def picture_size
+    if picture.size > 5.megabytes
+      errors.add(:picture, 'should be lower than 5MB')
+    end
+  end
 end
