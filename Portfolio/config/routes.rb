830619765c20e0f1cb78820d2d2478d159838788
zakevicius <m.zakevicius@gmail.com>
2021-02-16 22:10:17 +0200

    Added Project via resource generator

--- Portfolio/config/routes.rb
+++ Portfolio/config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   get 'pages/about'
   get 'pages/contact'
   resources :blogs
+  resources :projects
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
