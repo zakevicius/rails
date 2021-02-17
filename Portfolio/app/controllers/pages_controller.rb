14632ae86bb31a2b05a8b2d2f785967517b77db7
zakevicius <m.zakevicius@gmail.com>
2021-02-17 20:50:46 +0200

    Introduce rubocop

--- Portfolio/app/controllers/pages_controller.rb
+++ Portfolio/app/controllers/pages_controller.rb
@@ -1,10 +1,13 @@
+# frozen_string_literal: true
+
+require 'awesome_print'
+
 class PagesController < ApplicationController
   def home
+    ap Blog.all
   end
 
-  def about
-  end
+  def about; end
 
-  def contact
-  end
+  def contact; end
 end
