32b1185ecd02ac7a5f74569c88c3d4250028fc18
zakevicius <m.zakevicius@gmail.com>
2021-02-18 21:31:59 +0200

    New routes for pages

--- Portfolio/config/routes.rb
+++ Portfolio/config/routes.rb
@@ -1,10 +1,11 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get 'pages/home'
-  get 'pages/about'
-  get 'pages/contact'
+  root to: 'pages#home'
+
+  get 'about', to: 'pages#about'
+  get 'contact', to: 'pages#contact'
+
   resources :blogs
   resources :projects
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
