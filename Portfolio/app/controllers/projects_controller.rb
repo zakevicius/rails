e1e7a9a21c2752317eca54401b11f6d649a9807e
zakevicius <m.zakevicius@gmail.com>
2021-02-18 19:49:28 +0200

    Index action for projects

--- Portfolio/app/controllers/projects_controller.rb
+++ Portfolio/app/controllers/projects_controller.rb
@@ -1,2 +1,22 @@
 class ProjectsController < ApplicationController
+  
+  def index
+    @projects = Project.all
+  end
+
+  def new
+
+  end
+
+  def show
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
 end
