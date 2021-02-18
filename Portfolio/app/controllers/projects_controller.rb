01b7a487150e2fdd94f8e0b21439024bb7b543e9
zakevicius <m.zakevicius@gmail.com>
2021-02-18 20:13:26 +0200

    New and create actions for projects

--- Portfolio/app/controllers/projects_controller.rb
+++ Portfolio/app/controllers/projects_controller.rb
@@ -1,15 +1,26 @@
+# frozen_string_literal: true
+
 class ProjectsController < ApplicationController
-  
+  before_action :set_project, only: %[show, update, destroy]
+
   def index
     @projects = Project.all
   end
 
   def new
-
+    @project = Project.new
   end
 
-  def show
+  def show; end
 
+  def create
+    @project = Project.new(project_params)
+
+    if @project.save
+      redirect_to projects_path, notice: 'New project created!'
+    else
+      render 'new'
+    end
   end
 
   def update
@@ -18,5 +29,15 @@ class ProjectsController < ApplicationController
 
   def destroy
 
+  end
+
+  private
+
+  def set_project
+    @project = Project.find(params[:id])
+  end
+
+  def project_params
+    params.require(:project).permit(:title, :subtitle, :body)
   end
 end
