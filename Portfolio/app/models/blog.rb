a6ed4079275bfb219ef086198eaf99b65f73b13d
zakevicius <m.zakevicius@gmail.com>
2021-02-19 22:16:37 +0200

    Validations for models

--- Portfolio/app/models/blog.rb
+++ Portfolio/app/models/blog.rb
@@ -3,6 +3,8 @@
 class Blog < ApplicationRecord
   enum status: { draft: 0, published: 1 }
   extend FriendlyId
+
   friendly_id :title, use: :slugged
+  validates_presence_of :title, :body
   default_scope -> { order(id: :desc) }
 end
