995c5dbea8d0d5515010df02783d52bfdcd2d3a5
zakevicius <m.zakevicius@gmail.com>
2021-02-21 19:37:00 +0200

    Added concern for managing image data

--- Portfolio/app/models/project.rb
+++ Portfolio/app/models/project.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class Project < ApplicationRecord
+  includes Placeholder
   validates_presence_of :title, :body, :main_image, :thumb_image
 
   after_initialize :set_defaults
@@ -13,7 +14,7 @@ class Project < ApplicationRecord
   end
 
   def set_defaults
-    self.main_image ||= 'https://via.placeholder.com/600x400'
-    self.thumb_image ||= 'https://via.placeholder.com/350x200'
+    self.main_image ||= Placeholder.image_generator(height: '600', width: '400')
+    self.thumb_image ||= Placeholder.image_generator(height: '350', width: '200')
   end
 end
--- Portfolio/app/models/skill.rb
+++ Portfolio/app/models/skill.rb
@@ -1,5 +1,12 @@
 # frozen_string_literal: true
 
 class Skill < ApplicationRecord
+  includes Placeholder
   validates_presence_of :title, :percentage
+
+  after_initialize :set_defaults
+
+  def set_defaults
+    self.badge ||= Placeholder.image_generator(height: '250', width: '250')
+  end
 end
