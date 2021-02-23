c4e82c90eaa9f70886402711e058bab3a6b8c9b5
zakevicius <m.zakevicius@gmail.com>
2021-02-23 12:34:19 +0200

    Auth routes changed

--- Portfolio/config/routes.rb
+++ Portfolio/config/routes.rb
@@ -1,7 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
+
   root to: 'pages#home'
 
   get 'about', to: 'pages#about'
